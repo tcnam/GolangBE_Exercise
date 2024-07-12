1bc7ad77ea19ab5249759b869c2697cf529feaa6
Nam Tran <tcnam2000@gmail.com>
2024-07-12 10:10:34 +0700

    modify task4 in week1

--- week1/task4/task4.go
+++ week1/task4/task4.go
@@ -3,29 +3,24 @@ package main
 import "fmt"
 
 func two_sum(nums []int, target int) []int {
-	mymap := make(map[int]int)
+	var hash_map map[int]int = make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		j, ok := mymap[target-nums[i]]
+		j, ok := hash_map[target-nums[i]]
 		if ok {
 			result := []int{j, i}
 			return result
 		}
-		mymap[nums[i]] = i
+		hash_map[nums[i]] = i
 	}
 	result := []int{-1, -1}
 	return result
 }
 func main() {
-	var nums []int = []int{3, 2, 7, 11, 15}
+	var nums []int = []int{2, 7, 11, 15}
 	var target int = 9
-	for val := range two_sum(nums, target) {
-		fmt.Println(val)
-	}
+	fmt.Printf("%v", two_sum(nums, target))
 
 	var nums2 []int = []int{3, 2, 4}
 	var target2 int = 6
-	for val2 := range two_sum(nums2, target2) {
-		fmt.Println(val2)
-	}
-
+	fmt.Printf("%v", two_sum(nums2, target2))
 }
