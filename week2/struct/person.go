8dab19f546700df8c39789c3dc84c4ddddeffc35
tcnam <56421023+tcnam@users.noreply.github.com>
2024-07-17 10:44:19 +0700

    week2 tasks

--- week2/struct/person.go
+++ week2/struct/person.go
@@ -24,8 +24,24 @@ func (per *Person) check_match_with_job() bool {
 	}
 }
 
+func get_char_frequency(s string) map[rune]int {
+	var result_map map[rune]int = make(map[rune]int)
+	for _, character := range s {
+		if _, ok := result_map[character]; ok {
+			result_map[character] += 1
+		} else {
+			result_map[character] = 1
+		}
+	}
+	return result_map
+}
+
 func main() {
 	var per Person = Person{name: "Nam1", job: "Data engineer", date_of_birth: time.Date(2000, time.Month(2), 15, 0, 0, 0, 0, time.UTC)}
 	fmt.Println(per.calculate_age())
 	fmt.Println(per.check_match_with_job())
+
+	for key, value := range get_char_frequency("aaaaaaabcbbc") {
+		fmt.Printf("%c:%v\n", key, value)
+	}
 }
